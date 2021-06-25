6cabb867633577d520a31d51dc99695941693dc3
Dao Ngoc Huong <zdhuong@gmail.com>
2021-06-26 01:56:02 +0700

    [Add] Finish tutorial Ememies part 1 static observers

--- Training3D/Assets/Scripts/GameEnding.cs
+++ Training3D/Assets/Scripts/GameEnding.cs
@@ -1,7 +1,7 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
@@ -9,37 +9,52 @@ public class GameEnding : MonoBehaviour
     public float displayImageDuration = 1f;
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
 
     bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
     float m_Timer;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (m_IsPlayerAtExit)
-        {
-            EndLevel();
-        }
-    }
-
-    private void EndLevel()
-    {
-        m_Timer += Time.deltaTime;
-
-        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
-    }
-
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player)
         {
             m_IsPlayerAtExit = true;
+        }
+    }
+
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
+
+    void Update()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel(exitBackgroundImageCanvasGroup, false);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
+        }
+    }
+
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
+    {
+        m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
         }
     }
 }
